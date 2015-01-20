e14c0436bc09de65c4c39ca2847d944c1e0c9231
Ray Shan <noreply>
2015-01-20 17:42:02 +0000

    Add component ms-segments

---
+++ ui/ms-segments.reel/ms-segments.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/ms-segments.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MsSegments
+ * @extends Component
+ */
+exports.MsSegments = Component.specialize(/** @lends MsSegments# */ {
+    constructor: {
+        value: function MsSegments() {
+            this.super();
+        }
+    }
+});

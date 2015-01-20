e06b46887403c8395d44cdc3bfc858c6f1d410a0
Ray Shan <noreply>
2015-01-20 17:41:29 +0000

    Add component ms-segmented-bar

---
+++ ui/ms-segmented-bar.reel/ms-segmented-bar.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/ms-segmented-bar.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MsSegmentedBar
+ * @extends Component
+ */
+exports.MsSegmentedBar = Component.specialize(/** @lends MsSegmentedBar# */ {
+    constructor: {
+        value: function MsSegmentedBar() {
+            this.super();
+        }
+    }
+});
